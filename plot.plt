5deedb04fa34cd950a88af9e09bb10dfca3d9818
Softmatters <58212320+softmatter-design@users.noreply.github.com>
2022-12-02 17:50:38 +0900

    12/2

--- plot.plt
+++ plot.plt
@@ -1,32 +1,48 @@
-set term pngcairo font "Arial,14"
-
-set colorsequence classic
-
-data = "data.dat"
-
+set term pngcairo font "Arial,14" 
+set colorsequence classic 
+# 
+data = "data.dat" 
 set output "plot.png"
-
+#
 set size square
 set y2tics
 set logscale xyy2
-set xrange [1:100]
+
+#
+#set xrange [1e-2:1e5]
 set yrange [1e4:2e9]
 set y2range [1e-2:1e1]
 
+#
 set xlabel "Freq."
-set ylabel "tan{/Symbol d}"
+set ylabel "G"
+set y2label "tan{/Symbol d}"
 
-plot data ind 0 u 1:4 axis x1y2 w l lw 2 lt 1 ti "Tan", \
-data ind 0 u 1:2 axis x1y1 w l lw 2 lt 2 ti "G'", \
-data ind 0 u 1:3 axis x1y1 w l lw 2 lt 3 ti "G''", \
-data ind 1 u 1:4 axis x1y2 w l lw 2 lt 1 noti, \
-data ind 1 u 1:2 axis x1y1 w l lw 2 lt 2 noti, \
-data ind 1 u 1:3 axis x1y1 w l lw 2 lt 3 noti, \
-data ind 2 u 1:4 axis x1y2 w l lw 2 lt 1 noti, \
-data ind 2 u 1:2 axis x1y1 w l lw 2 lt 2 noti, \
-data ind 2 u 1:3 axis x1y1 w l lw 2 lt 3 noti, \
-data ind 3 u 1:4 axis x1y2 w l lw 2 lt 1 noti, \
-data ind 3 u 1:2 axis x1y1 w l lw 2 lt 2 noti, \
-data ind 3 u 1:3 axis x1y1 w l lw 2 lt 3 noti, \
+plot data ind 0 u 1:4 axis x1y2 w l lc 0 noti, \
+data ind 1 u 1:4 axis x1y2 w l lc 1 noti, \
+data ind 2 u 1:4 axis x1y2 w l lc 2 noti, \
+data ind 3 u 1:4 axis x1y2 w l lc 3 noti, \
+data ind 4 u 1:4 axis x1y2 w l lc 4 noti, \
+data ind 5 u 1:4 axis x1y2 w l lc 5 noti, \
+data ind 6 u 1:4 axis x1y2 w l lc 6 noti, \
+data ind 7 u 1:4 axis x1y2 w l lc 7 noti, \
+data ind 8 u 1:4 axis x1y2 w l lc 8 noti, \
+data ind 9 u 1:4 axis x1y2 w l lc 9 noti, \
+data ind 10 u 1:4 axis x1y2 w l lc 10 noti, \
+data ind 11 u 1:4 axis x1y2 w l lc 11 noti, \
+data ind 12 u 1:4 axis x1y2 w l lc 12 noti, \
+data ind 13 u 1:4 axis x1y2 w l lc 13 noti, \
+data ind 14 u 1:4 axis x1y2 w l lc 14 noti, \
+data ind 15 u 1:4 axis x1y2 w l lc 15 noti, \
+data ind 16 u 1:4 axis x1y2 w l lc 16 noti, \
+data ind 17 u 1:4 axis x1y2 w l lc 17 noti, \
+data ind 18 u 1:4 axis x1y2 w l lc 18 noti, \
+data ind 19 u 1:4 axis x1y2 w l lc 19 noti, \
+data ind 20 u 1:4 axis x1y2 w l lc 20 noti, \
+data ind 21 u 1:4 axis x1y2 w l lc 21 noti, \
+data ind 22 u 1:4 axis x1y2 w l lc 22 noti, \
+data ind 23 u 1:4 axis x1y2 w l lc 23 noti, \
+data ind 24 u 1:4 axis x1y2 w l lc 24 noti, \
+
 
 reset
--- read_csv.py
+++ read_csv.py
@@ -1,40 +1,111 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+#####
 import csv
-
+import platform
+import subprocess
+#####
 csvfile = "datafile.csv"
-csvfile_open = open(csvfile,"r",encoding="utf-8")
-csvfile_csvread =list(csv.reader(csvfile_open))
-csvfile_open.close()
-
 cols = [3, 9, 10, 12]
 n_data = 11
+base_temp = 130
+#####
+def readcsv():
+	datalist= csv_read(csvfile)
 
-datalist = []
+	set_shift(datalist, base_temp)
 
-for i, i_csvfile_csvread in enumerate(csvfile_csvread):
-    tmp = [[[] for i in cols] for j in range(n_data+1)]
-    if i_csvfile_csvread[0] == '測定点数':
-        temp = float(csvfile_csvread[i+2][2])
-        for col, item in enumerate(cols):
-            tmp[0][col] = i_csvfile_csvread[item]
-            for j in range(n_data):
-                # print(csvfile_csvread[i+2+j][item])
-                tmp[j+1][col] = csvfile_csvread[i+2+j][item]
-        datalist.append([round(temp), tmp])
+	make_datafiles(datalist)
 
-with open('data.dat','w') as f:
-    for each in sorted(datalist):
-        f.write('# Temp = ' + str(each[0]) +'\n')
-        for item in each[1][0]:
-            f.write('# ' + str(item) +'\t')
-        f.write('\n\n')
-        for line in each[1][1:]:
-            for data in line:
-                f.write(str(data) + '\t')
-            f.write('\n')
-        f.write('\n\n')
+	repeat = len(datalist)
+	make_multi_graph(repeat)
+	return
+#####
+def csv_read(csvfile):
+	with open(csvfile, encoding='utf8') as f:
+		csvread = list(csv.reader(f))
+	datalist = make_datalist(csvread)
+	return datalist
+
+def make_datalist(csvread):
+	datalist = []
+	for i, line_csvread in enumerate(csvread):
+		tmp = [[[] for i in cols] for j in range(n_data+1)]
+		if line_csvread[0] == '測定点数':
+			temp = float(csvread[i+2][2])
+			for col, item in enumerate(cols):
+				tmp[0][col] = line_csvread[item]
+				for j in range(n_data):
+					tmp[j+1][col] = csvread[i+2+j][item]
+			datalist.append([round(temp), tmp])
+	return datalist
+
+def set_shift(datalist, base_temp):
+	shift_dic = {each[0]:10**((base_temp - float(each[0]))/10) for each in datalist}
+	with open('shift.dat','w') as f:
+		f.write('#\tTemp\taT\tbT\n')
+		for each in sorted(datalist):
+			f.write(f'{each[0]}\t{shift_dic[each[0]]:.1e}\t1.0\n')
+	return
+
+def make_datafiles(datalist):
+	shift_dic = read_shift()
+	with open('data.dat','w') as f:
+		for each in sorted(datalist):
+			at = shift_dic[str(each[0])][0]
+			bt = shift_dic[str(each[0])][1]
+			f.write(f'# Temp = {each[0]}\t{at}\t{bt}\n# ')
+			for item in each[1][0]:
+				f.write(str(item) +'\t')
+			f.write('\n\n')
+			for line in each[1][1:]:
+				f.write(f'{float(line[0])*float(at):.2e}\t')
+				for data in line[1:3]:
+					f.write(f'{float(data)*float(bt):.2e}\t')
+				f.write(str(line[3]) + '\n')
+			f.write('\n\n')
+	return
+
+def read_shift():
+	shift_dic = {}
+	with open('shift.dat', 'r') as f:
+		shift_list = f.readlines()
+		for line in shift_list:
+			line_list = line.replace('\n', '').split('\t')
+			if line_list[0] != '#':
+				shift_dic[line_list[0]] = [line_list[1], line_list[2]]
+	return shift_dic
+
+# グラフを作成
+def make_multi_graph(repeat):
+	make_multi_script(repeat)
+	if platform.system() == "Windows":
+		subprocess.call('plot.plt', shell=True)
+	elif platform.system() == "Linux":
+		subprocess.call('gnuplot ' + 'plot.plt', shell=True)
+	return
+
+# 必要なスクリプトを作成
+def make_multi_script(repeat):
+	with open('plot.plt', 'w') as f:
+		script = multi_script_content(repeat)
+		f.write(script)
+	return
+
+# スクリプトの中身
+def multi_script_content(repeat):
+	script = 'set term pngcairo font "Arial,14" \nset colorsequence classic \n'
+	script += '# \ndata = "data.dat" \nset output "plot.png"\n'
+	script += '#\nset size square\nset y2tics\nset logscale xyy2\n\n'
+	script += '#\n#set xrange [1e-2:1e5]\nset yrange [1e4:2e9]\nset y2range [1e-2:1e1]\n\n'
+	script += '#\nset xlabel "Freq."\nset ylabel "G"\nset y2label "tan{/Symbol d}"\n\n'
+	#
+	script += 'plot '
+	for i in range(repeat):
+		script += 'data ind ' + str(i) + ' u 1:4 axis x1y2 w l lc ' + str(i) + ' noti, \\\n'
+	script += '\n\nreset'
+	return script
 
 
-
+if __name__ == '__main__':
+	readcsv()
