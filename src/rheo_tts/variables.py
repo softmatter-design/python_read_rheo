cbcbf8c3b3f66aba74da8052ca3b124f6b3d3540
Softmatters <58212320+softmatter-design@users.noreply.github.com>
2023-02-08 16:03:55 +0900

    02/08

---
+++ src/rheo_tts/variables.py
@@ -0,0 +1,21 @@
+csvfile = ''
+datalabel_dic = {
+			'Temp.':'温度', 
+			'Ang. Freq.':'角周波数', 
+			'Str. Mod.':'貯蔵弾性率', 
+			'Loss Mod.':'損失弾性率', 
+			'Tan_d':'損失正接',
+			}
+skip = 1
+
+temp_list = []
+binaryfile = ''
+moddata = {}
+
+shift_dic= {}
+shiftfile = ''
+c1 = 17.44
+c2 = 51.60
+tg = 300.0
+param_list = []
+ref_temp = 0.
